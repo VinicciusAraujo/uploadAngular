bfe4bfa9da8c891e33b828eb6849f33b00b1dc62
Vinícius Araújo <vinicciustreze2016@gmail.com>
2022-11-17 12:40:12 -0300

    Primeiro commit

---
+++ AngularFiles/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolbarComponent } from './compnt/toolbar/toolbar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import { InputComponent } from './compnt/input/input.component';
+import {MatMenuModule} from '@angular/material/menu';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ToolbarComponent,
+    InputComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatMenuModule,    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
